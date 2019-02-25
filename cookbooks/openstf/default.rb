4054d2ae05a9bb4ac007edaed68ed6da93611f68
Manabu Seki <github@zitaku.net>
2019-02-25 11:17:15 +0900

    rethinkdb追加

--- cookbooks/openstf/default.rb
+++ cookbooks/openstf/default.rb
@@ -5,3 +5,22 @@ end
 service 'adbd' do
     action [:enable, :restart]
 end
+
+case node[:platform]
+when 'redhat'
+  execute 'old docker remove' do
+    user "root"
+    command <<-EOC
+    docker run --rm -v /srv/rethinkdb:/data rethinkdb:2.3 rethinkdb --initial-password config[:keys][:rethinkdb_key]
+    EOC
+  end
+end
+
+template '/lib/systemd/system/rethinkdb.service' do
+    variables(rethinkdb_key: config[:keys][:rethinkdb_key])
+    source 'template/rethinkdb.service.erb'
+end
+
+service 'rethinkdb' do
+    action [:enable, :restart]
+end
